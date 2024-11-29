6ea1af535eb9235e468e21adc54655a7b891ba4e
LeeBumSeok <qpwoeiru6486@gmail.com>
2024-11-29 13:28:14 +0000

    Remove Submodule

--- FastV/demo.sh
+++
@@ -1,9 +0,0 @@
-model_path=models/llava-v1.5-7b
-
-python ./demo.py \
-        --model-path $model_path \
-        --use-fast-v \
-        --fast-v-sys-length 36 \
-        --fast-v-image-token-length 576 \
-        --fast-v-attention-rank 72 \
-        --fast-v-agg-layer 2
---
+++ SequentialPrune/src/setup_llava.sh
@@ -0,0 +1,6 @@
+cd LLaVA
+pip install -e .
+cd ../transformers
+pip install -e .
+pip install datasets seaborn pycocoevalcap
+pip install moviepy==1.0.3
